852a2ebbccf0f32cfd13e5b4a25c35101aa350b6
jinaduafeez <99200888+jinaduafeez@users.noreply.github.com>
2023-12-22 10:31:26 +0000

    Add files via upload
    My coursework 1

---
+++ data.js
@@ -0,0 +1,39 @@
+var product = [{
+    "id": 0,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "https://pngtree.com/freepng/whatsapp-icon-whatsapp-logo_3584844.html",
+
+
+    
+}, {
+    "id": 1,
+    "title": "Samsung",
+    "price": "70",
+    "description": "£",
+    "brand": "",
+    "image":"https://www.bing.com/ck/a?!&&p=07b113eb6acd9959JmltdHM9MTcwMzAzMDQwMCZpZ3VpZD0xMDFmNDM1NC00YTc2LTZlODctM2I4OS01MTVlNGI2NDZmZWImaW5zaWQ9NTUyNA&ptn=3&ver=2&hsh=3&fclid=101f4354-4a76-6e87-3b89-515e4b646feb&u=a1L2ltYWdlcy9zZWFyY2g_cT1zYW1zdW5nK2ltYWdlK2xpbmsmaWQ9QjUxQTdBMkI2Q0M5NDdEQjE4NjlCQjVCMEE2QzUyQTQ2Q0NGQjMwMyZGT1JNPUlRRlJCQQ&ntb=1",
+
+}, {
+    "id": 2,
+    "title": "iphone",
+    "price": "£100",
+    "description": "",
+    "brand": "iphone",
+    "image":"https://photos5.appleinsider.com/gallery/50639-100007-iPhone-14-Plus-On-End-Closer-xl.jpg",
+}, {
+
+}, {
+    "id": 3,
+    "title": "Tecno",
+    "price": "£120",
+    "description": "",
+    "brand": "",
+    "image":"https://fdn2.gsmarena.com/vv/bigpic/tecno-spark-7.jpg",
+}, 
+
+
+
+]
